c2408139b3b614c882e8bed3e3e9a3a83b964449
YaSh1543 <yash1543jain@gmail.com>
2025-08-07 19:37:23 +0530

    Update main.tf

--- microservices/booking-service/terraform/main.tf
+++ microservices/booking-service/terraform/main.tf
@@ -2,20 +2,16 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"  # Pins to AWS provider version 4.x
+      version = "~> 4.0"  
     }
   }
 }
 
-# AWS Provider Configuration
+
 provider "aws" {
   region = "ap-south-1"  # Mumbai region
   
-  # Optional: Uncomment if you need to specify a specific AWS profile
-  # profile = "your-aws-profile-name"
-  
-  # Optional: Uncomment if you want to assume a specific role
-  # assume_role {
-  #   role_arn = "arn:aws:iam::ACCOUNT_ID:role/ROLE_NAME"
-  # }
+ 
+   }
 }
+
